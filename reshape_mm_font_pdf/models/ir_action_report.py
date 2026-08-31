282c07c6508c7133c3f8ed21e755ec3f7f5c6509
S.W.Nwe <suwynnwe@gmail.com>
2026-08-31 09:07:29 +0630

    optimise beautiBeautifulSoup usage

--- reshape_mm_font_pdf/models/ir_action_report.py
+++ reshape_mm_font_pdf/models/ir_action_report.py
@@ -12,14 +12,13 @@ class IrActionsReport(models.Model):
         html_string = html.decode("utf-8") if is_bytes else html
 
         soup = BeautifulSoup(html_string, "html.parser")
-
         myanmar_regex = re.compile(r"[\u1000-\u109F\uAA60-\uAA7F\uA9E0-\uA9FF]+")
 
         def reshape_myanmar_only(match):
             return reshape_myanmar_word(match.group(0))
 
-        for text_node in soup.find_all(text=True):
-            if text_node.parent.name in ["script", "style"]:
+        for text_node in soup.find_all(string=True):
+            if text_node.parent and text_node.parent.name in ["script", "style"]:
                 continue
 
             if myanmar_regex.search(text_node):
@@ -27,6 +26,5 @@ class IrActionsReport(models.Model):
                 text_node.replace_with(new_text)
 
         reshaped_html = str(soup)
-
         prepared_html = reshaped_html.encode("utf-8") if is_bytes else reshaped_html
         return super()._prepare_html(prepared_html, report_model)
